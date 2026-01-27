a2c09827fb20d9313190de4958f9b23e976ff665
jonsetzky <40266374+jonsetzky@users.noreply.github.com>
2026-01-27 14:57:47 +0200

    create basic food page

--- app/src/main/java/com/mobilecomputing/MainActivity.kt
+++ app/src/main/java/com/mobilecomputing/MainActivity.kt
@@ -12,6 +12,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.mobilecomputing.ui.theme.MobileComputingTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.ui.res.painterResource
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+
+const val LOREM_IPSUM_1301_CHARS = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna."
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +31,39 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MobileComputingTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                FoodPage(
+                    "A cheesecake",
+                    LOREM_IPSUM_1301_CHARS
                     )
-                }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun FoodImage(modifier: Modifier = Modifier) {
+    Image(painter = painterResource(R.drawable.cheesecake),
+        contentDescription = "A cheesecake",
+        modifier)
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MobileComputingTheme {
-        Greeting("Android")
+fun FoodPage(
+    title: String,
+    textBody: String,
+) {
+    Column(
+        modifier = Modifier.verticalScroll(rememberScrollState())
+    ) {
+        FoodImage()
+        Text(text = title,
+            style = MaterialTheme.typography.titleLarge,
+            fontSize = 8.em,
+            modifier = Modifier.padding(all = 12.dp).padding(bottom = 0.dp)
+        )
+        Text(text = textBody,
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.padding(all = 12.dp)
+        )
     }
 }
