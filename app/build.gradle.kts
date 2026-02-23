65ce84a7ad456bf261cc307ecd2734f144e9c092
jonsetzky <40266374+jonsetzky@users.noreply.github.com>
2026-02-23 08:29:47 +0200

    add room.schemaLocation and v1 schema

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,3 +78,7 @@ dependencies {
     implementation ("androidx.room:room-runtime:${room_version}")
     ksp("androidx.room:room-compiler:$room_version")
 }
+
+ksp {
+    arg("room.schemaLocation", "$projectDir/schemas")
+}
